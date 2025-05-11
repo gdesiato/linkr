56d424c6b35402a0e1509eabbec0e3cb0a8fbd30
Giuseppe Desiato <giodesiato@gmail.com>
2025-05-11 20:13:20 +0200

    added domain

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { DomainListComponent } from './domain/domain-list/domain-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'domains', component: DomainListComponent }
+];
---
+++ frontend/src/app/domain/domain-list/domain-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-domain-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './domain-list.component.html',
+  styleUrl: './domain-list.component.scss'
+})
+export class DomainListComponent {
+
+}
